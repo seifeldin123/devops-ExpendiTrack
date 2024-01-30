4694a12f616cf40b031ca7754eba30f847451e71
seifeldineid <seifeldin.eid@compunneldigital.com>
2024-01-30 12:22:08 -0500

    Initial Commit

---
+++ backend/Tracker/src/test/java/BudgetTracker/Tracker/service/BudgetServiceTest.java
@@ -0,0 +1,16 @@
+package BudgetTracker.Tracker.service;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
+
+@DataJpaTest
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=password",
+        "spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect"
+})
+public class BudgetServiceTest {
+
+}
