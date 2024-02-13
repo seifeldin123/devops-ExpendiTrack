69048d45707a1cde3c83d62a522e532065921190
sanyadrian <adrianovoleksandr@gmail.com>
2024-02-13 13:06:05 -0500

    Testing for Budget and User Exceptions

--- backend/src/main/java/BudgetTracker/Tracker/service/BudgetService.java
+++ backend/src/main/java/BudgetTracker/Tracker/service/BudgetService.java
@@ -51,6 +51,6 @@ public class BudgetService {
         return budgetRepository.save(budget);
     }
     private boolean isValidAlphanumeric(String name) {
-        return name.matches("^(?=.*[a-zA-Z])[a-zA-Z0-9]+$");
+        return name!=null && name.matches("^(?=.*[a-zA-Z])[a-zA-Z0-9]+$");
     }
 }
