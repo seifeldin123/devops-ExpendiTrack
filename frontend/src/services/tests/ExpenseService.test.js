2b2a715a5facbb7f7045d0bbbf3411baa461daee
Harmeet Matharoo <hmjatt@gmail.com>
2024-02-15 16:20:24 -0500

    adjust test numbering because of removed tests
    Signed-off-by: Harmeet Matharoo <hmjatt@gmail.com>

--- frontend/src/services/tests/ExpenseService.test.js
+++ frontend/src/services/tests/ExpenseService.test.js
@@ -22,14 +22,14 @@ describe('ExpenseService', () => {
         };
 
         const mockResponse = {
-                expensesId: 1,
-                ...expenseData,
-                budget: {
-                    budgetId: 1,
-                    budgetDescription: null,
-                    budgetAmount: 0,
-                    user: null
-                }
+            expensesId: 1,
+            ...expenseData,
+            budget: {
+                budgetId: 1,
+                budgetDescription: null,
+                budgetAmount: 0,
+                user: null
+            }
         };
         axios.post.mockResolvedValue( {response: { status: 201, mockResponse } });
 
@@ -69,36 +69,36 @@ describe('ExpenseService', () => {
     });
 
 
-    // TC_UI_005: Verify Successful Expense Retrieval by User ID
-    it('TC_UI_005: should successfully retrieve expenses by user ID', async () => {
+    // TC_UI_004: Verify Successful Expense Retrieval by User ID
+    it('TC_UI_004: should successfully retrieve expenses by user ID', async () => {
         const userId = 1;
         const mockResponse =
-        [
-            {
-                expensesId: 1,
-                expensesDescription: "Office Supplies",
-                expensesAmount: 100,
-                expensesDate: "2024-02-06T10:00:00Z",
-                budget: {
-                    budgetId: 5,
-                    budgetDescription: "School",
-                    budgetAmount: 500,
-                    user: {
-                        id: 1,
-                        name: "Jane",
-                        email: "jane@example.com"
+            [
+                {
+                    expensesId: 1,
+                    expensesDescription: "Office Supplies",
+                    expensesAmount: 100,
+                    expensesDate: "2024-02-06T10:00:00Z",
+                    budget: {
+                        budgetId: 5,
+                        budgetDescription: "School",
+                        budgetAmount: 500,
+                        user: {
+                            id: 1,
+                            name: "Jane",
+                            email: "jane@example.com"
+                        }
                     }
                 }
-            }
-        ]
+            ]
 
         axios.get.mockResolvedValue({response: {status: 200, mockResponse}});
 
         await expect(getUserExpenses(userId)).resolves.toEqual(mockResponse.data);
     });
 
-    // TC_UI_006: Verify Expense Retrieval Failure due to Nonexistent User
-    it('TC_UI_006: should handle failure due to nonexistent user', async () => {
+    // TC_UI_005: Verify Expense Retrieval Failure due to Nonexistent User
+    it('TC_UI_005: should handle failure due to nonexistent user', async () => {
 
         const errorMessage = '[]';
         axios.get.mockRejectedValue({response: {data: errorMessage}});
@@ -106,8 +106,8 @@ describe('ExpenseService', () => {
         await expect(getBudgetsByUserId(999)).rejects.toThrow(errorMessage);
     });
 
-    // TC_UI_007: Verify Expense Creation Failure due to Invalid Description
-    it('TC_UI_007: should handle failure due to invalid expense description', async () => {
+    // TC_UI_006: Verify Expense Creation Failure due to Invalid Description
+    it('TC_UI_006: should handle failure due to invalid expense description', async () => {
         const expenseData = {
             expensesDescription: "!!!***",
             expensesAmount: 50,
@@ -122,8 +122,8 @@ describe('ExpenseService', () => {
         await expect(createExpense(expenseData)).rejects.toThrow(errorMessage);
     });
 
-    // TC_UI_008: Verify Expense Creation Failure due to Duplicate Expense Name
-    it('TC_UI_008: should handle failure due to duplicate expense name', async () => {
+    // TC_UI_007: Verify Expense Creation Failure due to Duplicate Expense Name
+    it('TC_UI_007: should handle failure due to duplicate expense name', async () => {
 
         const expenseData = {
             expensesDescription: "Office Supplies",
