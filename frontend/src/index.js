30c5f41ee327e685bad176811e6bbfd01e326110
sanyadrian <adrianovoleksandr@gmail.com>
2024-02-26 14:31:24 -0500

    i18n configuration

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,9 +3,31 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/index.css';
 
+import {I18nextProvider} from "react-i18next";
+
+import i18next from "i18next";
+import en from "./translations/en/common.json";
+import fr from "./translations/fr/common.json";
+
+i18next.init({
+    interpolation: { escapeValue: false },
+    lng:"en",
+    resources: {
+        en: {
+            global:en
+        },
+        fr: {
+            global:fr
+        },
+    }
+});
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <I18nextProvider i18n={i18next}>
+            <App />
+        </I18nextProvider>
     </React.StrictMode>
 );
