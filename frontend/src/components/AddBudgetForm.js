caac09508a4d93ca4280f8fc5b8a831d61b5f114
sanyadrian <adrianovoleksandr@gmail.com>
2024-02-27 14:31:48 -0500

    AddBudgetForm modified

--- frontend/src/components/AddBudgetForm.js
+++ frontend/src/components/AddBudgetForm.js
@@ -99,13 +99,13 @@ const AddBudgetForm = ({ existingBudget = null, onClose }) => {
             <form onSubmit={handleSubmit}>
                 <section className="panel panel-primary">
                     <header className="panel-heading">
-                        <h2 className="panel-title">{existingBudget ? 'Edit Budget' : 'Create Budget'}</h2>
+                        <h2 className="panel-title">{existingBudget ? 'Edit Budget' : t("app.add-budget-create-budget-title")}</h2>
                     </header>
                     <div className="form-section-container">
                         <div className="form-group mrgn-tp-sm">
 
                             <label htmlFor="budget-description">
-                                <span className="field-name">Budget Name</span> <strong
+                                <span className="field-name">{t("app.add-budget-budget-name")}</span> <strong
                                 className="required">(required)</strong>
                             </label>
 
@@ -141,20 +141,20 @@ const AddBudgetForm = ({ existingBudget = null, onClose }) => {
                         {existingBudget ? (
                             <button type="submit" className="btn-lg btn-success">
                                 <span className="glyphicon glyphicon-floppy-save"></span>
-                                &nbsp; Update Budget
+                                &nbsp; {t("app.budgetItem-edit")}
                             </button>
                         ) : (
                             <div className="mrgn-bttm-md">
                                 <button type="submit" className="btn-lg btn-default">
                                     <span className="glyphicon glyphicon-plus"></span>
-                                    &nbsp;Create Budget
+                                    &nbsp;{t("app.add-budget-create-budget-title")}
                                 </button>
                             </div>
                         )}
 
                         {error && (
                             <div className="alert alert-danger" role="alert">
-                                <h4>The form could not be submitted because these errors were found:</h4>
+                                <h4>{t("app.budgetItem-the-form-cannot-be-submitted")}</h4>
                                 <ul>
                                     <li>{error}</li>
                                 </ul>
@@ -166,7 +166,7 @@ const AddBudgetForm = ({ existingBudget = null, onClose }) => {
                                     <button type="button" className="close" data-dismiss="alert" aria-label="Close" onClick={() => setShowSuccessAlert(false)}>
                                         <span aria-hidden="true">&times;</span>
                                     </button>
-                                    <p>Budget successfully {existingBudget ? 'updated' : 'added'}! </p>
+                                    <p>{t("app.add-budget-successfully")} {existingBudget ? t("app.add-budget-updated") : t("app.add-budget-added")}! </p>
                                 </div>
                             )
                         }
